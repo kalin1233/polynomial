35408a199b6b1895f79d3d5dbd025b2f34281bea
kalin1233 <kalin32@bu.edu>
2024-01-26 22:47:41 -0500

    part b
    cs 506 excercise part b

--- polynomial.py
+++ polynomial.py
@@ -26,14 +26,40 @@ class Mul:
         self.p2 = p2
     
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
+        if isinstance(self.p1, (Add, Sub, Div)):
+            if isinstance(self.p2, (Add, Sub, Div)):
                  return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
+        if isinstance(self.p2, (Add, Sub, Div)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        
+    def __repr__(self):
+        if isinstance(self.p1, (Add, Mul, Sub)):
+            if isinstance(self.p2, (Add, Mul, Sub)):
+                return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        if isinstance(self.p2, (Add, Mul, Sub)):
+            return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+        
+        #Handles zero division
+        if isinstance(self.p2, Int) and self.p2.i == 0:
+            return "Undefined"
+        return "( " + repr(self.p1) + " ) / " + repr(self.p2)
 
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+class Sub: 
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        
+    def __repr__(self):
+        return repr(self.p1) + "-" + repr(self.p2)
+
+poly = Add( Div( Int(4), Int(3)), Mul( X(), Mul( Int(1), Div( Mul(X(), X()), Int(1)))))
 print(poly)
