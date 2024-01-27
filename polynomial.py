7a89b900ee3457cda25602a604fbb9f5c909866b
kalin1233 <kalin32@bu.edu>
2024-01-27 11:51:50 -0500

    cs 506 excercise part c
    part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,6 +42,10 @@ class Mul:
         if isinstance(self.p2, (Add, Sub, Div)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+
     
 class Div:
     def __init__(self, p1, p2):
@@ -51,6 +64,9 @@ class Div:
         if isinstance(self.p2, Int) and self.p2.i == 0:
             return "Undefined"
         return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
 
 class Sub: 
@@ -60,6 +76,9 @@ class Sub:
         
     def __repr__(self):
         return repr(self.p1) + "-" + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 
-poly = Add( Div( Int(4), Int(3)), Mul( X(), Mul( Int(1), Div( Mul(X(), X()), Int(1)))))
-print(poly)
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
